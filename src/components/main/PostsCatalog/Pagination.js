52be38b8afcb4a962f8d866b78934a3fd78a2890
axobadzegiorgi5 <axobadzegiorgi5@gmail.com>
2022-10-24 13:07:24 +0400

    pagination buttons show active

--- src/components/main/PostsCatalog/Pagination.js
+++ src/components/main/PostsCatalog/Pagination.js
@@ -1,19 +1,33 @@
+const Pagination = ({
+  totalPosts,
+  postsPerPage,
+  changeCurrentPage,
+  currentPage,
+}) => {
+  let pages = [];
 
-const Pagination = ({totalPosts, postsPerPage, changeCurrentPage}) => {
-    let pages = [];
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pages.push(i);
+  }
 
-    for(let i = 1; i <= Math.ceil(totalPosts/postsPerPage); i++) {
-        pages.push(i)
-    }
-
-    return (
-        <div className="pagination__buttons">{
-            pages.map((page, index) => {
-                
-                return <button className="pagination-btn" key={index} onClick={() => changeCurrentPage(page)}>{page}</button>
-            })
-            }</div>
-    )
-}
+  return (
+    <div className="pagination__buttons">
+      {pages.map((page, index) => {
+        console.log(page, currentPage);
+        return (
+          <button
+            className={`pagination-btn ${
+              currentPage == page ? "active-page" : ""
+            }`}
+            key={index}
+            onClick={() => changeCurrentPage(page)}
+          >
+            {page}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Pagination;
